7ad164d5e822f9ad39fab38ad398da6a508c1f1d
Mike Waites <mikey.waites@gmail.com>
2018-02-16 10:49:26 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='arrested',
-    version='0.1.1',
+    version='0.1.2',
     author='Mikey Waites',
     author_email='mike@oldstlabs.com',
     url='https://github.com/mikeywaites/flask-arrested',
